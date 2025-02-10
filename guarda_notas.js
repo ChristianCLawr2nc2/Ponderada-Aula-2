10b57740ca65e1b1fcc8e31237609052e86943ea
Christian de Carvalho Lawrence <christian.lawrence@sou.inteli.edu.br>
2025-02-10 12:02:54 -0300

    Add files via upload

---
+++ guarda_notas.js
@@ -0,0 +1,25 @@
+var nota1 = 6
+console.log(nota1)
+var nota2 = 8
+console.log(nota2)
+var nota3 = 5
+console.log(nota3)
+
+var media = (nota1 + nota2 + nota3) / 3
+console.log(media);
+
+if(media >= 7){
+    console.log('Aprovado');
+}
+
+else if(media < 7){
+    console.log('Infelizente você não foi Aprovado');
+}
+
+if(media < 7){
+    console.log(Recuperação);
+}
+
+else if(media < 7){
+    console.log('Sua media foi muito, você está Reprovado');
+}
